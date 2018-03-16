0c2dd4ec2e853320848573447ff63c517a50154b
NiShiAn <30385099+NiShiAn@users.noreply.github.com>
2018-03-16 19:57:53 +0800

    require.js

---
+++ Test/scripts/plus/plusMain.js
@@ -0,0 +1,29 @@
+﻿require.config({
+    // baseUrl: "/scripts/plus",//
+    //deps: ['require-css'],
+    paths: {
+        //"require-css": "css.min",
+        jquery: "//libs.baidu.com/jquery/2.0.3/jquery",
+        pinyin_dict_firstletter: "pinyin/dict/pinyin_dict_firstletter",
+        pinyin_dict_withtone: "pinyin/dict/pinyin_dict_withtone",
+        pinyin: "pinyin/pinyinUtil",
+        ngy: "test"
+        //select2: "select2/select2.min"
+    },
+    //map: {
+    //    '*': {
+    //        css: '/scripts/plus/css.min.js'
+    //    }
+    //},
+    shim: {
+        ngy: {
+            exports: "NGY"
+        },
+        pinyin: {
+            deps: ["pinyin_dict_firstletter", "pinyin_dict_withtone"],
+            exports: "pinyin"
+        },
+        //select2: ["/scripts/plus/select2/select2.min.css"]
+    },
+
+});
---
+++ Test/scripts/plus/test.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    var NGY = {
+        Hi: function() {
+            alert("跨越过去之后");
+        }
+    }
+    window.NGY = NGY;
+})();
